65d033908e0b09854c85fdbdd4cc79552eedbbc0
WeihanLi <weihanli@outlook.com>
2019-06-10 17:50:55 +0800

    update DependenceResolverExtension.cs

---
+++ src/WeihanLi.Web.Extensions/DependenceResolverExtension.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using WeihanLi.Common;
+
+namespace WeihanLi.Web.Extensions
+{
+    public static class DependenceResolverExtension
+    {
+        public static TService ResolveCustomService<TService>([NotNull]this IDependencyResolver dependencyResolver)
+        {
+            var contextAccessor = dependencyResolver.GetService<IHttpContextAccessor>();
+            if (contextAccessor != null)
+            {
+                return contextAccessor.HttpContext.RequestServices.GetService<TService>();
+            }
+            return dependencyResolver.GetService<TService>();
+        }
+    }
+}
