8ce917bdf822e4b9c331e7d1138d5b4a188cb02d
WeihanLi <weihanli@outlook.com>
2019-06-26 19:10:50 +0800

    update GetUserIP

--- src/WeihanLi.Web.Extensions/HttpContextExtension.cs
+++ src/WeihanLi.Web.Extensions/HttpContextExtension.cs
@@ -11,9 +11,9 @@ namespace WeihanLi.Web.Extensions
         /// GetUserIP
         /// </summary>
         /// <param name="httpContext">httpContext</param>
-        /// <param name="realIPHeader">realIPHeader, default `X-Real-IP`</param>
-        /// <returns></returns>
-        public static string GetUserIP(this HttpContext httpContext, string realIPHeader = "X-Real-IP")
+        /// <param name="realIPHeader">realIPHeader, default `X-Forwarded-For`</param>
+        /// <returns>user ip</returns>
+        public static string GetUserIP(this HttpContext httpContext, string realIPHeader = "X-Forwarded-For")
         {
             return httpContext.Request.Headers.TryGetValue(realIPHeader, out var ip) ? ip.ToString() : httpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
         }
