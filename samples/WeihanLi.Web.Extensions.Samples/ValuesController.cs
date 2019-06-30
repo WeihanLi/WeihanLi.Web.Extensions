a47d55ce0b27b5c155de1c0d658fa1a58f4555f3
weihanli <weihanli@outlook.com>
2019-06-30 19:25:30 +0800

    add ValuesController

---
+++ samples/WeihanLi.Web.Extensions.Samples/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeihanLi.Web.Extensions.Samples
+{
+    [Route("/api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var userId = User.GetUserId<int>();
+
+            return Ok(new[] { 1, 2, 3 });
+        }
+    }
+}
