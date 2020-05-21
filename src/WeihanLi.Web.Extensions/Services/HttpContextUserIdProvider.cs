55ad738397641280e24106da3f29b6a8f55b0532
weihanli <weihanli@outlook.com>
2020-05-21 23:00:22 +0800

    add HttpContextUserIdProviderOptions, make HttpContextUserIdProvider more friendly for di

--- src/WeihanLi.Web.Extensions/Services/HttpContextUserIdProvider.cs
+++ src/WeihanLi.Web.Extensions/Services/HttpContextUserIdProvider.cs
@@ -1,28 +1,41 @@
 ﻿using System;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using WeihanLi.Common.Services;
 using WeihanLi.Web.Extensions;
 
 namespace WeihanLi.Web.Services
 {
+    public sealed class HttpContextUserIdProviderOptions
+    {
+        private Func<HttpContext, string> _userIdFactory = context => context?.User?.GetUserId();
+
+        public Func<HttpContext, string> UserIdFactory
+        {
+            get => _userIdFactory;
+            set
+            {
+                if (value != null)
+                {
+                    _userIdFactory = value;
+                }
+            }
+        }
+    }
+
     public sealed class HttpContextUserIdProvider : IUserIdProvider
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly Func<HttpContext, string> _userIdFactory;
 
         public HttpContextUserIdProvider(
-            IHttpContextAccessor httpContextAccessor
-        ) : this(httpContextAccessor, null)
-        { }
-
-        public HttpContextUserIdProvider(
             IHttpContextAccessor httpContextAccessor,
-            Func<HttpContext, string> userIdFactory
+            IOptions<HttpContextUserIdProviderOptions> options
             )
         {
             _httpContextAccessor = httpContextAccessor;
 
-            _userIdFactory = userIdFactory ?? (context => context?.User?.GetUserId());
+            _userIdFactory = options.Value.UserIdFactory;
         }
 
         public string GetUserId()
