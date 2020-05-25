e058b02bfca160e38af4650031ffeaf2c91db4a2
weihanli <weihanli@outlook.com>
2020-05-25 18:05:26 +0800

    add ResultModelExtensions.cs

---
+++ src/WeihanLi.Web.Extensions/Extensions/ResultModelExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using WeihanLi.Common.Models;
+
+namespace WeihanLi.Web.Extensions
+{
+    public static class ResultModelExtensions
+    {
+        public static IActionResult GetActionResult(this ResultModel resultModel)
+        {
+            if (resultModel == null)
+                return new NoContentResult();
+
+            if (resultModel.Status == ResultStatus.RequestError)
+                return new BadRequestObjectResult(resultModel);
+
+            if (resultModel.Status == ResultStatus.MethodNotAllowed)
+                return new ObjectResult(resultModel)
+                {
+                    StatusCode = (int)HttpStatusCode.MethodNotAllowed
+                };
+
+            if (resultModel.Status == ResultStatus.Unauthorized)
+                return new UnauthorizedObjectResult(resultModel);
+
+            if (resultModel.Status == ResultStatus.NoPermission)
+                return new ObjectResult(resultModel)
+                {
+                    StatusCode = (int)HttpStatusCode.Forbidden
+                };
+
+            return new OkObjectResult(resultModel);
+        }
+    }
+}
