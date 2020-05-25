4eb7030ff87c3ebb8adeaae0c5c93bb41b48996c
weihanli <weihanli@outlook.com>
2020-05-25 17:51:53 +0800

    add HttpContextLoggingEnricher

---
+++ src/WeihanLi.Web.Extensions/Services/HttpContextLoggingEnricher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using WeihanLi.Common.Logging;
+using WeihanLi.Web.Extensions;
+
+namespace WeihanLi.Web.Services
+{
+    public class HttpContextLoggingEnricher : ILogHelperLoggingEnricher
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly Action<LogHelperLoggingEvent, HttpContext> _enrichAction;
+
+        public HttpContextLoggingEnricher(IHttpContextAccessor contextAccessor) : this(contextAccessor, null)
+        {
+        }
+
+        public HttpContextLoggingEnricher(IHttpContextAccessor contextAccessor, Action<LogHelperLoggingEvent, HttpContext> enrichAction)
+        {
+            _contextAccessor = contextAccessor;
+            if (enrichAction == null)
+            {
+                _enrichAction = (logEvent, httpContext) =>
+                {
+                    logEvent.AddProperty("RequestIP", httpContext.GetUserIP());
+                    logEvent.AddProperty("RequestPath", httpContext.Request.Path);
+                    logEvent.AddProperty("RequestMethod", httpContext.Request.Method);
+
+                    logEvent.AddProperty("TraceId", httpContext.Request.Headers["TraceId"].ToString());
+                    logEvent.AddProperty("Referer", httpContext.Request.Headers["Referer"].ToString());
+                };
+            }
+            else
+            {
+                _enrichAction = enrichAction;
+            }
+        }
+
+        public void Enrich(LogHelperLoggingEvent loggingEvent)
+        {
+            if (null != _contextAccessor?.HttpContext)
+            {
+                _enrichAction.Invoke(loggingEvent, _contextAccessor.HttpContext);
+            }
+        }
+    }
+}
