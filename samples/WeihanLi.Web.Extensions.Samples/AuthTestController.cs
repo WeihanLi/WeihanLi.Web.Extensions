6c01c7122336034aa44b1390d9f71b0cc5033fa5
Weihan Li <weihanli@outlook.com>
2022-03-07 00:00:15 +0800

    Add AuthTestController

---
+++ samples/WeihanLi.Web.Extensions.Samples/AuthTestController.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.AspNetCore.Mvc;
+using WeihanLi.Web.Extensions.Samples.Filters;
+
+namespace WeihanLi.Web.Extensions.Samples;
+
+[TestAuthFilter]
+[Route("api/authTest")]
+public class AuthTestController : ControllerBase
+{
+    [TestAuthFilter]
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
---
+++ samples/WeihanLi.Web.Extensions.Samples/Filters/TestAuthFilter.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.AspNetCore.Mvc.Filters;
+using WeihanLi.Web.Filters;
+
+namespace WeihanLi.Web.Extensions.Samples.Filters;
+
+public class TestAuthFilter : AuthorizationFilterAttribute
+{
+    public override void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!context.IsEffectivePolicy(this)) return;
+        Console.WriteLine($"{nameof(TestAuthFilter)} is executing");
+    }
+}
