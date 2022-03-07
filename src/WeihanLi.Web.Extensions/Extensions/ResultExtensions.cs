d998e2b04b19e138a19804bfb4b519fd6b7ea12d
Weihan Li <weihanli@outlook.com>
2022-03-07 21:21:30 +0800

    Upgrade common

--- src/WeihanLi.Web.Extensions/Extensions/ResultExtensions.cs
+++ src/WeihanLi.Web.Extensions/Extensions/ResultExtensions.cs
@@ -9,6 +9,45 @@ namespace WeihanLi.Web.Extensions;
 
 public static class ResultModelExtensions
 {
+    public static IActionResult GetOkOResult(this Result resultModel)
+    {
+        return resultModel is null
+            ? new OkResult()
+            : new OkObjectResult(resultModel);
+    }
+
+    public static IActionResult GetRestResult(this Result resultModel)
+    {
+        if (resultModel == null)
+            return new NoContentResult();
+
+        if (resultModel.Status == ResultStatus.RequestError)
+            return new BadRequestObjectResult(resultModel);
+
+        if (resultModel.Status == ResultStatus.ResourceNotFound)
+            return new NotFoundObjectResult(resultModel);
+
+        if (resultModel.Status == ResultStatus.MethodNotAllowed)
+            return new ObjectResult(resultModel)
+            {
+                StatusCode = (int)HttpStatusCode.MethodNotAllowed
+            };
+
+        if (resultModel.Status == ResultStatus.Unauthorized)
+            return new ObjectResult(resultModel)
+            {
+                StatusCode = (int)HttpStatusCode.Unauthorized
+            };
+
+        if (resultModel.Status == ResultStatus.NoPermission)
+            return new ObjectResult(resultModel)
+            {
+                StatusCode = (int)HttpStatusCode.Forbidden
+            };
+
+        return new OkObjectResult(resultModel);
+    }
+
     public static IActionResult GetOkOResult(this ResultModel resultModel)
     {
         return resultModel is null
