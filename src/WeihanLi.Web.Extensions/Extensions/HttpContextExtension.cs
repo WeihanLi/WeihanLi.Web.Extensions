3dc682d7335f716f740e11d5deb7da8c2975577d
Weihan Li <weihanli@outlook.com>
2022-03-28 23:23:14 +0800

    Fix HttpContextExtension warnings

--- src/WeihanLi.Web.Extensions/Extensions/HttpContextExtension.cs
+++ src/WeihanLi.Web.Extensions/Extensions/HttpContextExtension.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Weihan Li. All rights reserved.
 // Licensed under the MIT license.
 
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using WeihanLi.Extensions;
 
@@ -14,6 +15,7 @@ public static class HttpContextExtension
     /// <param name="httpContext">httpContext</param>
     /// <param name="realIPHeader">realIPHeader, default `X-Forwarded-For`</param>
     /// <returns>user ip</returns>
+    // ReSharper disable InconsistentNaming
     public static string GetUserIP(this HttpContext httpContext, string realIPHeader = "X-Forwarded-For")
     {
         if (httpContext is null)
@@ -23,11 +25,11 @@ public static class HttpContextExtension
 
         return httpContext.Request.Headers.TryGetValue(realIPHeader, out var ip)
             ? ip.ToString()
-            : httpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+            : httpContext.Connection.RemoteIpAddress?.MapToIPv4().ToString();
     }
 
     /// <summary>
-    /// GetUserId from claims, get from `nameid`/ClaimTypes.NameIdentifier by default
+    /// GetUserId from claims, get from `ClaimTypes.NameIdentifier/`nameid`/`sub` by default
     /// </summary>
     /// <param name="principal">principal</param>
     /// <returns></returns>
@@ -38,38 +40,25 @@ public static class HttpContextExtension
         {
             return userId;
         }
-        userId = GetUserId(principal, "nameid");
+        userId = GetUserId(principal, JwtRegisteredClaimNames.NameId);
         if (!string.IsNullOrEmpty(userId))
         {
             return userId;
         }
-        return GetUserId(principal, "sub");
+        return GetUserId(principal, JwtRegisteredClaimNames.Sub);
     }
 
     /// <summary>
-    /// GetUserId from claims, get from `nameid`/ClaimTypes.NameIdentifier by default
+    /// GetUserId from claims, get from ClaimTypes.NameIdentifier/`nameid`/`sub` by default
     /// </summary>
     /// <typeparam name="T">userId type</typeparam>
     /// <param name="principal">principal</param>
     /// <returns></returns>
-    public static T GetUserId<T>(this ClaimsPrincipal principal)
-    {
-        if (typeof(T) == typeof(string))
-            return (T)(object)principal.GetUserId();
+    public static T GetUserId<T>(this ClaimsPrincipal principal) => GetUserId(principal).ToOrDefault<T>();
 
-        return GetUserId(principal).ToOrDefault<T>();
-    }
+    public static T GetUserId<T>(this ClaimsPrincipal principal, string claimType) 
+        => GetUserId(principal, claimType).ToOrDefault<T>();
 
-    public static T GetUserId<T>(this ClaimsPrincipal principal, string claimType)
-    {
-        return GetUserId(principal, claimType).ToOrDefault<T>();
-    }
-
-    public static string GetUserId(this ClaimsPrincipal principal, string claimType)
-    {
-        if (principal?.HasClaim(c => c.Type == claimType) == true)
-            return principal.FindFirst(claimType).Value;
-
-        return null;
-    }
+    public static string GetUserId(this ClaimsPrincipal principal, string claimType) 
+        => principal.FindFirst(claimType)?.Value;
 }
